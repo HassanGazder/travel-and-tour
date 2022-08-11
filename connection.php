0aaaf8d4fac33e4851264f8a653944982328a5df
hassangazder <hassangazder3@gmail.com>
2022-08-11 12:53:51 +0500

    links added

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$userserver = "localhost";
-$username = "root";
-$userpassword = "";
-$userdb = "department";
-$connection = mysqli_connect($userserver,$username,$userpassword,$userdb);
-
-?>
